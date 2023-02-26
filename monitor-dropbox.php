4110cd7a57007c8385d9ccaf42cb95bad8ca30b7
davidduyker <dduyker@gmail.com>
2023-02-26 00:02:17 -0800

    Filter Dropbox contents in php

--- monitor-dropbox.php
+++ monitor-dropbox.php
@@ -1,31 +1,38 @@
 <?php
+$dropbox_url = 'https://www.dropbox.com/sh/87qg7860raqqeaf/AAAuDdu_6fNB3dg_HZje-VR4a?raw=1'; // Dropbox URL
+$output_file = './dropbox.zip';
 
-$dropbox_url = 'https://www.dropbox.com/sh/87qg7860raqqeaf/AAAuDdu_6fNB3dg_HZje-VR4a?raw=1'; // replace with your Dropbox URL
-$output_dir = '../liveimage'; // replace with the directory where you want to save the extracted image
+// Check if output directory is writable, if not set appropriate permissions
+if (!is_writable(dirname($output_file))) {
+    chmod(dirname($output_file), 0755);
+}
 
 // download the zip file
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $dropbox_url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close($ch);
 
 // save the zip file to disk
-$temp_file = tempnam(sys_get_temp_dir(), 'dropbox_zip');
-file_put_contents($temp_file, $data);
+file_put_contents($output_file, $data);
 
-// extract the contents of the zip file
+// extract the zip file contents
 $zip = new ZipArchive;
-$res = $zip->open($temp_file);
+$res = $zip->open($output_file);
 if ($res === TRUE) {
-    $zip->extractTo($output_dir);
+    // iterate through the files in the archive and extract only the images
+    for ($i = 0; $i < $zip->numFiles; $i++) {
+        $filename = $zip->getNameIndex($i);
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+        if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif'])) {
+            $zip->extractTo('./', $filename);
+        }
+    }
     $zip->close();
     echo 'Zip file extracted successfully';
 } else {
-    echo 'Failed to extract zip file';
+    echo 'Error: Could not open the zip file';
 }
-
-// delete the temporary zip file
-unlink($temp_file);
-
 ?>
