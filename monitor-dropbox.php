98bc5b85bf913e776862db0fafecf3ed7799dcd0
davidduyker <dduyker@gmail.com>
2023-02-25 22:48:52 -0800

    New url for dropbox in php

--- monitor-dropbox.php
+++ monitor-dropbox.php
@@ -1,6 +1,6 @@
 <?php
 
-$dropbox_url = 'https://www.dropbox.com/sh/87qg7860raqqeaf/AAAuDdu_6fNB3dg_HZje-VR4a?dl=1'; // replace with your Dropbox URL
+$dropbox_url = 'https://www.dropbox.com/sh/87qg7860raqqeaf/AAAuDdu_6fNB3dg_HZje-VR4a?raw=1'; // replace with your Dropbox URL
 $output_dir = '../liveimage'; // replace with the directory where you want to save the extracted image
 
 // download the zip file
@@ -9,13 +9,10 @@ curl_setopt($ch, CURLOPT_URL, $dropbox_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 curl_close($ch);
-echo 'Downloaded data: ' . $data . "\n";
 
 // save the zip file to disk
 $temp_file = tempnam(sys_get_temp_dir(), 'dropbox_zip');
-echo 'Temporary file path: ' . $temp_file . "\n";
 file_put_contents($temp_file, $data);
-echo 'Saved data to file: ' . file_get_contents($temp_file) . "\n";
 
 // extract the contents of the zip file
 $zip = new ZipArchive;
