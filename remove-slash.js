7e7d7ef430f8dabb756258207729a5becfa232d7
David Duyker <dduyker@gmail.com>
2025-01-31 07:16:29 -0800

    more in depth 2023 links and image url fix

--- remove-slash.js
+++
@@ -1,7 +0,0 @@
-if (window.location.pathname.endsWith('/') && window.location.pathname !== '/') {
-    // Store that we've already tried to redirect
-    if (!sessionStorage.getItem('redirected')) {
-        sessionStorage.setItem('redirected', 'true');
-        window.location.href = window.location.href.slice(0, -1);
-    }
-}
